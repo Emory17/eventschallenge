3072119833fa86e3123b10af733f49f1e7f8aa90
Emory Soper <emorysoper@icloud.com>
2023-04-19 16:37:46 -0400

    edit

--- js/site.js
+++ js/site.js
@@ -107,7 +107,8 @@ function displayEventData(currentEvents){
         tableRow.querySelector("[data-id='city']").textContent = event.city;
         tableRow.querySelector("[data-id='state']").textContent = event.state;
         tableRow.querySelector("[data-id='attendance']").textContent = event.attendance.toLocaleString();
-        tableRow.querySelector("[data-id='date']").textContent = new Date(event.date).toLocaleDateString();
+        tableRow.querySelector("[data-id='date']").textContent = (new Date(event.date)).toLocaleDateString();
+        tableRow.querySelector("tr").setAttribute("data-event", event.id);
         eventTable.appendChild(tableRow);
     }
 }
@@ -153,10 +154,22 @@ function getEventData(){
     let data = localStorage.getItem("es-eventdata");
 
     if(data == null){
-        localStorage.setItem("es-eventdata", JSON.stringify(events))
+        let identifiedEvents = events.map(event => {
+            event.id = generateId();
+            return event;
+        })
+
+        localStorage.setItem("es-eventdata", JSON.stringify(identifiedEvents));
+        data = localStorage.getItem("es-eventdata");
     }
 
-    let currentEvents = data == null ? events : JSON.parse(data);
+    let currentEvents = JSON.parse(data);
+
+    if(currentEvents.some(event => event.id == undefined)){
+        currentEvents.forEach(event => event.id = generateId());
+        localStorage.setItem("es-eventdata", JSON.stringify(currentEvents));
+    }
+
     return currentEvents;
 }
 
@@ -195,12 +208,81 @@ function saveNewEvent(){
         city: city,
         state: state,
         attendance: attendance,
-        date: date
+        date: date,
+        id: generateId()
     }
 
     let events = getEventData();
     events.push(newevent);
 
+    localStorage.setItem("es-eventdata", JSON.stringify(events));
+    buildDropDown();
+    document.getElementById("newEventForm").reset();
+}
+
+function generateId() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+            c ^
+            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+    );
+}
+
+function editEvent(eventRow){
+    let eventId = eventRow.getAttribute('data-event');
+    let currentEvents = getEventData();
+    let event = currentEvents.find((eventObject) => eventObject.id == eventId);
+
+    document.getElementById("editid").value = event.id;
+    document.getElementById("editname").value = event.event;
+    document.getElementById("editcity").value = event.city;
+
+    let editState = document.getElementById("editstate");
+    editState.selectedIndex = [...editState.options].findIndex(option => option.text == event.state);
+
+    document.getElementById("editattend").value = event.attendance;
+
+    let eventDate = (new Date(event.date)).toISOString().split("T")[0];
+    document.getElementById("editdate").value = eventDate;
+}
+
+function deleteEvent(){
+    let eventId = document.getElementById("editid").value;
+    let events = getEventData();
+    events = events.filter(event => event.id != eventId);
+    localStorage.setItem("es-eventdata", JSON.stringify(events));
+    buildDropDown();
+}
+
+function updateEvent(){
+    let eventId = document.getElementById("editid").value;
+    let events = getEventData();
+    let eventindex = events.findIndex(event => event.id == eventId);
+
+    let name = document.getElementById("editname").value;
+    let city = document.getElementById("editcity").value;
+
+    let stateSelect = document.getElementById("editstate");
+    let selectedIndex = stateSelect.selectedIndex;
+    let state = stateSelect.options[selectedIndex].text;
+
+    let attendance = parseInt(document.getElementById("editattend").value);
+
+    let dateValue = document.getElementById("editdate").value;
+    dateValue = new Date(dateValue);
+    let date = dateValue.toLocaleDateString();
+
+    let newevent = {
+        event: name,
+        city: city,
+        state: state,
+        attendance: attendance,
+        date: date,
+        id: eventId
+    };
+
+    events[eventindex] = newevent;
     localStorage.setItem("es-eventdata", JSON.stringify(events));
     buildDropDown();
 }
